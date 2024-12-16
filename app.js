6fc280428a95de0545c3bc9dd1f21dd15098b469
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-17 00:51:00 +0500

    Updated

--- app.js
+++ app.js
@@ -1,20 +1,79 @@
 
 
-// ======Chapter No1( Q1)======
-
+// ======Chapter No1(Q.1)======
 // alert ("Error! Please enter a valid password.");
 
-
-// ======Chapter No1( Q2)======
-
+// ======Chapter No1(Q.2)======
 //  alert ("Welcome to JS Land...\nHappy coding");
 
-
-// ======Chapter No1( Q3)======
-
+// ======Chapter No1(Q.3)======
 // alert ("Welcomr to JS Land...")
 
+// ======Chapter No1(Q.4)======
+// alert ("Hello... I can run JS through my web browser's console");
 
-// ======Chapter No1( Q1)======
 
-// alert ("Hello... I can run JS through my web browser's console")
+
+// =====chapter No2=====
+
+// ======Chapter No2(Q.1)======
+// var username = ("Muhammad Usman");
+
+// ======Chapter No2(Q.2)======
+// var myName = ("Muhammad Waqar");
+
+// ======Chapter No2(Q.3)======
+// var message = ("Hello World");
+// alert (message);
+
+// ======Chapter No2(Q.4)======
+// var name = ("jhone doe");
+// var age = ("15 year old");
+// var courseComplete = ("Certified Mobile Application Development");
+// alert (name);
+// alert (age);
+// alert (courseComplete);
+
+// ======Chapter No2(Q.5)======
+// var pizza = ("Pizza\npizz\npiz\npi\np");
+// alert (pizza);
+
+// ======Chapter No2(Q.6)======
+// var email = ("waqar.iqbal789@gmail.com");
+// var emailRef=("My email address is ");
+// var emailAlert = (emailRef + email);
+// alert (emailAlert);
+
+// ======Chapter No2(Q.7)======
+// var book = ("A smarter way to learn JavaScript");
+// alert ("I am trying to learn from a Book " + book);
+
+// ======Chapter No2(Q.8)======
+// var content = ("content through JavaScript");
+// document.write ("Yah! I Can write HTML " + content);
+
+// ======Chapter No2(Q.9)======
+// var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert (design);
+
+
+// =====chapter No3=====
+
+// ======Chapter No3(Q.1)======
+// var im = ("I em ");
+// var age = (15);
+// var year = (" year Old");
+// alert (im + age + year);
+
+// ======Chapter No3(Q.2)======
+// var visited = ("You have visite this site ");
+// var num = (4);
+// var time = (" times")
+// alert (visited + num + time);
+
+// ======Chapter No3(Q.3)======
+var birthYear = (1990);
+document.write("My birth year is "+ birthYear);
+document.write(" Dat type my ddeclared variable is number");
+// ======Chapter No3(Q.4)======
+
