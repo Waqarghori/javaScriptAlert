4b366c415d35895a6fac9210dd983c613d1f32f0
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-16 21:15:26 +0500

    Update

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+// ======Chapter No1( Q1)======
+
+// alert ("Error! Please enter a valid password.");
+
+
+// ======Chapter No1( Q2)======
+
+//  alert ("Welcome to JS Land...\nHappy coding");
+
+
+// ======Chapter No1( Q3)======
+
+// alert ("Welcomr to JS Land...")
+
+
+// ======Chapter No1( Q1)======
+
+// alert ("Hello... I can run JS through my web browser's console")
